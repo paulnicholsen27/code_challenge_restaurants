d647a3c3c425b8222c0f445354bf7258f6dce0ab
s-satsangi <s.satsangi@gmail.com>
2020-10-16 11:31:32 -0400

    Sharad Satsangi's Submission
    Submission for code challenge 10/16/2020

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
+  @@all = []
+  
   attr_accessor :given_name, :family_name
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def reviews
+    Review.all.select{|review| review.customer == self}
+  end
+
+  def restaurants
+    #Review.all.select{|review| review.customer == self}
+    self.reviews.map{|review| review.restaurant}.uniq
+  end
+
+  def num_reviews
+    self.reviews.length
+  end
+
+  def self.find_by_name(search_name)
+    Customer.all.find{|customer| customer.full_name == search_name}
+  end
+
+  def self.find_all_by_given_name(search_name)
+    Customer.all.select{|customer| customer.full_name == search_name}
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,20 @@
 class Review
-  
+
+    @@all = []
+
+    attr_reader    :customer, :restaurant, :rating
+
+    def initialize (customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
 end
