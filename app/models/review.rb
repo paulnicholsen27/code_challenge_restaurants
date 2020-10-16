3d837ca3d4cb095b061e6d8bf0a3c141287d5bd4
Erick-EEV <EEVERICKEEV@gmail.com>
2020-10-16 12:07:41 -0400

    Code Chanllenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Review
+    
+    @@all = []
+
+    attr_accessor :customer, :restaurant, :rating
+    
+    def initialize(customer, restaurant, rating=0)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
   
 end
