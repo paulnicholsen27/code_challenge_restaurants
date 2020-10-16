9ed906252593f1b7f16a449b7658837a6738c953
jrcliff <justoon7.jc@gmail.com>
2020-10-16 11:47:33 -0400

    couldnt get the average to work but feeling pretty good

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    save
   end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map do |review|
+      review.customer
+    end.uniq
+  end
+
+  def average_star_rating
+    Review.all.average(:rating) do |review|
+      review.restaurant == self
+      
+    end
+  end
+      
+
+  
+
+
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,24 @@
 class Review
+    attr_reader :customer, :restaurant
+    attr_accessor :rating
+
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    
   
 end
