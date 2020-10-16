b230b7a4f6613a8a480e1b3687c645b0b67afd59
Bryce Moore <brycemooore@gmail.com>
2020-10-16 10:18:39 -0400

    Finished code challenge

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,14 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+john = Customer.new("John", "Doe")
+jack = Customer.new("Jack", "The Ripper")
+paul = Customer.new("Paul", "Rudd")
+jack2 = Customer.new("Jack", "Johnson")
+
+faang = Restaurant.new("Faang Thai")
+chipotle = Restaurant.new("Chipotle")
+dominoes = Restaurant.new("Dominoes")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
