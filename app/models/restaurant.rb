0aacf07d7a0c6bfff57a9881ea27a37c2d58642d
Akram_Alam <arundelfootball23@gmail.com>
2020-10-16 11:51:32 -0400

    new stuff for yu paul

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,38 @@
 class Restaurant
-  attr_reader :name
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+      @name = name
 
-  def initialize(name)
-    @name = name
-  end
+      @@all << self 
+    end
+
+    def self.all 
+      @@all 
+    end 
+
+    def reviews
+      Review.all.select do |review| 
+        review.restaurant == self 
+        
+      end 
+    end 
+
+    def customers
+      Review.all.select do |review|
+        review.customer ==  self 
+     
+      end 
+    end 
+    
+    def average_star_rating
+      Review.all.select do |review|
+        review.rating / review.rating.count 
+        binding.pry 
+        
+      end 
+    end 
+
+    
 
 end
